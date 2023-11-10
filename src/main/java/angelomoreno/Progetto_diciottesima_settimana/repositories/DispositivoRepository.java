741db05584725b6d0fbc1bbda0b0c04f6aa56818
Angelo Moreno <emilioangelo665@gmail.com>
2023-11-10 11:33:44 +0100

    Agg. repositories delle entities

---
+++ src/main/java/angelomoreno/Progetto_diciottesima_settimana/repositories/DispositivoRepository.java
@@ -0,0 +1,7 @@
+package angelomoreno.Progetto_diciottesima_settimana.repositories;
+
+import angelomoreno.Progetto_diciottesima_settimana.entities.Dispositivo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DispositivoRepository extends JpaRepository<Dispositivo, Integer> {
+}
---
+++ src/main/java/angelomoreno/Progetto_diciottesima_settimana/repositories/UtenteRepository.java
@@ -0,0 +1,9 @@
+package angelomoreno.Progetto_diciottesima_settimana.repositories;
+
+import angelomoreno.Progetto_diciottesima_settimana.entities.Utente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UtenteRepository extends JpaRepository<Utente, Integer> {
+}
