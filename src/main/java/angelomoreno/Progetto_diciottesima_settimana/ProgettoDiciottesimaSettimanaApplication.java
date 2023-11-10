9e4d4bc03dfd3a77e99a46084bf79cdc883ecf9f
Angelo Moreno <emilioangelo665@gmail.com>
2023-11-10 10:15:32 +0100

    Creazione progetto

---
+++ src/main/java/angelomoreno/Progetto_diciottesima_settimana/ProgettoDiciottesimaSettimanaApplication.java
@@ -0,0 +1,13 @@
+package angelomoreno.Progetto_diciottesima_settimana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoDiciottesimaSettimanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoDiciottesimaSettimanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/angelomoreno/Progetto_diciottesima_settimana/ProgettoDiciottesimaSettimanaApplicationTests.java
@@ -0,0 +1,13 @@
+package angelomoreno.Progetto_diciottesima_settimana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoDiciottesimaSettimanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
